e48a005867a94dcd006b5b6aca05d001b00f1055
root <root@ubuntu.(none)>
2014-06-03 00:11:18 +0200

    updating 03_...py after stdp experiments
    Signed-off-by: root <root@ubuntu.(none)>

--- 03_twoNeurons.py
+++ 03_twoNeurons.py
@@ -8,21 +8,28 @@ import pylab as pl
 
 nest.ResetKernel()
 dt = 1.
-w_ss_nrn = 7.0
+w_ss_nrn = 6.0
 
 neurons = nest.Create('iaf_cond_exp',2)
 populationPoisson = nest.Create('poisson_generator',2)
 
-nest.SetStatus([populationPoisson[0]], {'rate' : 4500.0})
-nest.SetStatus([populationPoisson[1]], {'rate' : 4500.0})
+nest.SetStatus([populationPoisson[0]], {'rate' : 3000.0})
+#nest.SetStatus([populationPoisson[1]], {'rate' : 3000.0})
 
-nest.SetStatus([neurons[0]], {'V_reset': -70.0},{'t_ref': 500.0})
-#print nest.GetDefaults('iaf_cond_exp')
+nest.SetStatus([neurons[0]], {'t_ref': 2.0})
+print nest.GetDefaults('iaf_cond_exp')
 
 nest.Connect([populationPoisson[0]], [neurons[0]], params={'weight': w_ss_nrn})
 nest.Connect([populationPoisson[1]], [neurons[1]], params={'weight': w_ss_nrn})
 
-#nest.Connect([neurons[0]],[neurons[1]], model = 'stdp_synapse')
+print nest.GetDefaults('stdp_synapse')
+nest.CopyModel('stdp_synapse', 'inhibitory', {'weight': w_ss_nrn,
+                                              'delay': 1.})
+                                              
+nest.Connect([neurons[0]],[neurons[1]], model = 'inhibitory')
+
+nest.help('stdp_synapse')
+#nest.SetStatus()
 
 voltmeter = nest.Create('multimeter', params={'record_from': ['V_m']})
 nest.SetStatus(voltmeter,{"withtime": True})
@@ -36,7 +43,7 @@ spike_recorder = nest.Create('spike_detector')
 nest.ConvergentConnect(neurons, spike_recorder)
 
 
-nest.Simulate(1000.0)
+nest.Simulate(600.0)
 
 data = nest.GetStatus(voltmeter)
 data2 = nest.GetStatus(voltmeter2)
@@ -46,4 +53,3 @@ pl.plot(data[0]['events']['times'], data[0]['events']['V_m'])
 pl.subplot(212)
 pl.plot(data2[0]['events']['times'], data2[0]['events']['V_m'])
 pl.show()
-
